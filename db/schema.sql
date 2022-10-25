f5ae161d84d2a324276b6faec91f5f73f8d96a9a
Jeremiah Swearingen <jswearingen18@gmail.com>
2022-10-25 07:53:00 -0500

    updated schema.sql to refernce manager id

--- db/schema.sql
+++ db/schema.sql
@@ -4,28 +4,27 @@ CREATE DATABASE employees_db;
 USE employees_db;
 
 CREATE TABLE department(
-    id INT NOT NULL,
-    department_name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    id INT PRIMARY KEY NOT NULL,
+    department_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE employee_role(
-    id INT NOT NULL,
+    id INT PRIMARY KEY NOT NULL ,
     department_id INT NOT NULL,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8,2) NOT NULL,
     FOREIGN KEY (department_id)
-    REFERENCES department(id),
-    PRIMARY KEY (id)
+    REFERENCES department(id)
 );
 
 CREATE TABLE employee(
-    id INT NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
+    id INT PRIMARY KEY NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES employee_role(id),
-    PRIMARY KEY (id)
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -8,13 +8,13 @@ VALUES (10, 100, "Front-End Developer", 100000.00),
        (11, 100, "Back-End Developer", 110000.00),
        (12, 100, "Full-Stack Developer", 125000.00),
        (20, 101, "Database Administrator", 105000.00),
-       (21, 101, "Program Tester", 090000.00),
+       (21, 101, "Program Tester", 90000.00),
        (30, 102, "SoftWare Engineer", 140000.00),
        (31, 102, "Hardware Engineer", 140000.00),
        (32, 102, "Computer Engineer", 165000.00);
 
-INSERT INTO employee (id, role_id, manager_id, first_name, last_name)
-VALUES (1000, 12, NULL, "Hama", "Nephthys"),
-       (1001, 12, 1, "Ninsun", "Eogan"),
-       (1002, 30, NULL, "Neptuno", "Janaina"),
-       (1003, 21, NULL, "Sunita", "Dazhdbog");
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1000,"Hama", "Nephthys", 12, NULL),
+       (1001,"Ninsun", "Eogan", 12, 1000),
+       (1002, "Neptuno", "Janaina", 30, 1000),
+       (1003, "Sunita", "Dazhdbog", 21, 1000);
